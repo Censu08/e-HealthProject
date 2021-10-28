86a32c71b96d5294e25442923ff5d8e95b34b450
cataw <walter.catalfamo@mail.polimi.it>
2021-10-29 00:56:49 +0200

    a

--- Benchmark.py
+++ Benchmark.py
@@ -1,49 +1,71 @@
 import pandas as pd
 
 # We take the final dataset of serious games found with our algorithm and we extract the column "App Id"
-df_sg = pd.read_csv(r'C:\Users\Gilles FACCIN\Desktop\POLIMI 2021-2022\E-HEALTH METHODS\PROJECT\dataset_seriousgames.csv', sep = ",")
+df_sg = pd.read_csv(r'dataset_seriousgames.csv', sep = ",")
 ID_sg = df_sg["App Id"]
 
-# We take the benchmark dataset and we extract the fake games, the random apps and the serious games as lists
-df_bm = pd.read_csv(r'C:\Users\Gilles FACCIN\Desktop\POLIMI 2021-2022\E-HEALTH METHODS\PROJECT\benchmark_data.csv', sep = ";")
+# We take the benchmark dataset and we extract the fake games, the random apps and the serious games
+df_bm = pd.read_csv(r'benchmark_data.csv', sep = ";")
 fake_games = df_bm["FAKE GAMES"]
 random_apps = df_bm["RANDOM APPS"]
 serious_games = df_bm["SERIOUS GAMES"]
 
 # Important values
-TP = 0                 # True positives
-FN = 0                 # False negatives
-FP = 0                 # False positives
-TN = 0                 # True negatives
-nb_misleading = 0      # Number of misleading serious games detected as serious games
-nb_undetected_ra = 0   # Number of random apps falsely detected as serious games
+seriousGamesInDataset = 0
+seriousGamesNotInDataset = 0
+randomAppsInDataset = 0
+randomAppsNotInDataset = 0
+fakeGamesInDataset = 0
+fakeGamesNotInDataset = 0
 
-for i in range(len(serious_games)):
-    if serious_games[i] in ID_sg:
-        TP +=1
-    else:
-        FN += 1
-for i in range(len(random_apps)):
-    if random_apps[i] in ID_sg:
-        FP += 1
-        nb_undetected_ra += 1
-    else:
-        TN += 1
-for i in range(len(fake_games)):
-    if fake_games[i] in ID_sg:
-        FP +=1
-        nb_misleading += 1
-    else:
-        TN +=1
+for i in range(len(serious_games)-1):
+    flag = False
+    for j in range(len(ID_sg)):
+        if serious_games[i] == ID_sg[j]:
+            seriousGamesInDataset +=1
+            flag = True
+            break
+    if flag == False:
+        seriousGamesNotInDataset += 1
+for i in range(len(random_apps)-1):
+    flag = False
+    for j in range(len(ID_sg)):
+        if random_apps[i] == ID_sg[j]:
+            randomAppsInDataset += 1
+            flag = True
+            break
+    if flag == False:
+        randomAppsNotInDataset += 1
+for i in range(len(fake_games)-1):
+    flag = False
+    for j in range(len(ID_sg)):
+        if fake_games[i] == ID_sg[j]:
+            fakeGamesInDataset += 1
+            flag = True
+            break
+    if flag == False:
+        fakeGamesNotInDataset += 1
+
+#sensitivity = TP / (TP + FN)
+#specificity = TN / (TN + FP)
 
 # Formulas to evaluate the performance
-accuracy = (TP + TN)/(TP + TN + FP + FN)
-relative_accuracy = (TP + nb_misleading - nb_undetected_ra)/(TP + TN + FP + FN)
-sensitivity = TP/(TP + FN)
-specificity = TN/(TN + FP)
+accuracy = (seriousGamesInDataset + fakeGamesNotInDataset + randomAppsNotInDataset) /\
+           (seriousGamesInDataset + seriousGamesNotInDataset +
+            fakeGamesInDataset + fakeGamesNotInDataset +
+            randomAppsInDataset + randomAppsNotInDataset)
+effective_accuracy = ((seriousGamesInDataset + fakeGamesNotInDataset + randomAppsNotInDataset) - (seriousGamesNotInDataset + fakeGamesInDataset + randomAppsInDataset)) / \
+                     (seriousGamesInDataset + seriousGamesNotInDataset +
+                     fakeGamesInDataset + fakeGamesNotInDataset +
+                     randomAppsInDataset + randomAppsNotInDataset)
+sensitivity = seriousGamesInDataset /\
+              (seriousGamesInDataset + seriousGamesNotInDataset)
+specificity = (fakeGamesNotInDataset + randomAppsNotInDataset) /\
+              (fakeGamesNotInDataset + randomAppsNotInDataset + fakeGamesInDataset + randomAppsInDataset)
 
 # Results
-print("Accuracy: ", accuracy)
-print("Relative accuracy: ", relative_accuracy)
-print("Sensitivity: ", sensitivity)
-print("Specificity: ", specificity)
+print("Accuracy: ", round(accuracy * 100, 1), "%")
+print("Relative Accuracy: ", round(effective_accuracy * 100, 1), "%")
+print("Sensitivity: ", round(sensitivity * 100, 1), "%")
+print("Specificity: ", round(specificity * 100, 1), "%")
+
---
+++ Recognizer.py
@@ -0,0 +1,171 @@
+### Phase I: Identification of serious games for kids
+
+import json
+import play_scraper
+import pandas as pd
+from google_play_scraper import app
+import re
+
+#Filter this dataset with all the educational and family-related categories + specific features
+def filter(df):
+    df = df[((df["Category"] == "Education")
+                 | (df["Category"] == "Educational")
+                 | (df["Category"] == "Family")
+                 | (df["Category"] == "Learn")
+                 | (df["Category"] == "4 year old kids")
+                 | (df["Category"] == "4 year olds"))
+                & (df["Rating"] >= 4)
+                & (df["Rating Count"] >= 50000)]
+    return df
+
+
+##select only the apps from the dataset that are games
+def select_games(df):
+    problematic_apps = []
+    not_games = []
+    for i in range(len(df)):
+        try:
+            app_edu = app(df.iloc[i]["App Id"])
+            json_app_edu = json.dumps(app_edu)
+            json_app_edu = json.loads(json_app_edu)
+            cat_app = json_app_edu["genreId"]
+            if cat_app[:4] != "GAME":
+                not_games.append(i)
+        except:
+            #print(i)
+            problematic_apps.append(i)
+            continue
+    df = df.drop(index=problematic_apps, axis=0)
+    df_g = df.drop(index=not_games, axis=0)
+    return df_g
+
+##delete the apps without a clear age range nor learning category
+def filter_non_enrichable(df):
+    df = df[((df["Learning_category"] != "")
+                 & (df["Age_range"] != ""))]
+    return df
+
+## enrich the dataset
+def enrich_dataframe(df):
+    desc_app = []
+    rev_app = []
+    for i in range(len(df)):
+        app_edu = app(df.iloc[i]["App Id"])
+        json_app_edu = json.dumps(app_edu)
+        json_app_edu = json.loads(json_app_edu)
+        desc = json_app_edu["description"]
+        rev = json_app_edu["reviews"]
+        desc_app.append(desc)
+        rev_app.append(rev)
+    df["Description"] = desc_app
+    df["Reviews"] = rev_app
+    return df
+
+
+## Use of NLP to enrich the dataset with more detailed features
+
+
+
+def find_lc(df):
+    # Complete learning category
+    # List of learning categories
+    learning_cat = ["science", "counting", "language", "creativity", "shape", "food", "music", "sport"]
+    # List of keywords per category
+    science = ["science", "nature", "physics", "chemistry", "biology", "mathematics", "plants", "informatics",
+               "programming", "star", "planet", "galaxy", "scientific", "robot", "life", "laboratory", "virus"]
+    counting = ["counting", "number", "addition", "substraction", "division", "multiplication", "add", "fraction",
+                "mathematics", "sum", "product"]
+    language = ["language", "english", "spanish", "chinese", "vocabulary", "writing", "reading", "grammar"
+                                                                                                 "translation",
+                "linguistic", "speech", "slang", "mother tongue", "billingual", "latin"]
+    creativity = ["creativity", "drawing", "color", "design", "imagination", "idea", "painting", "pencil", "artist"]
+    shape = ["shape", "triangle", "rectangle", "square", "round", "circle", "cube", "sphere", "icon"]
+    food = ["food", "health", "nutrition", "ingredients", "cook", "meal", "vegetable", "fruit", "meat", "drink"]
+    music = ["music", "instrument", "compose", "sound", "partition", "lyric", "song", "dance", "concert",
+             "soundtrack", "MP3", "folk", "rock", "hip hop", "jazz", "pop", "echo"]
+    sport = ["sport", "ball", "run", "coach", "score", "train", "team", "shot"]
+
+    # List of all keywords combined
+    keywords_cat = [science, counting, language, creativity, shape, food, music, sport]
+    lc = []
+    for i in range(len(df)):
+        desc = df.iloc[i]["Description"]
+        Mx = 0
+        cat = ""
+        for j in range(len(keywords_cat)):
+            m = 0
+            nb = []
+            for k in range(len(keywords_cat[j])):
+                nb.append(desc.count(keywords_cat[j][k]))
+            m = max(nb)
+            if m > Mx:
+                Mx = m
+                cat = learning_cat[j]
+        lc.append(cat)
+    df["Learning_category"] = lc
+    return df
+
+
+
+
+def find_age_range(df):
+    # Complete the age range
+    # List of age ranges
+    age_range = ["babies", "children", "adolescents", "adults"]
+    # List of keywords per category
+    babies = ["baby", "babies", "1 year-old", "2 year-old", "3 year-old", "infant", "newborn",
+              "nursery"]  # 0-3 year-old
+    children = ["child", "children", "kid", "4 year-old", "5 year-old", "6 year-old", "7 year-old", "8 year-old",
+                "9 year-old", "10 year-old", "11 year-old", "12 year-old", "toddler", "elementary school",
+                "childhood", "preschool"]  # 4-12 year-old
+    adolescents = ["teenage", "adolescent", "middle school", "highschool", "13 year-old", "14 year-old", "15 year-old",
+                   "16 year-old", "17 year-old", "18 year-old", "19 year-old"]  # 13-19 year-old
+    adults = ["adult", "student", "university", "old", "middle-aged", "20 year-old", "mature",
+              "grown-up", "majority"]  # 20 and more year-old
+
+    # List of all keywords combined
+    keywords_age = [babies, children, adolescents, adults]
+    ar = []
+    for i in range(len(df)):
+        desc = df.iloc[i]["Description"]
+        Mx = 0
+        age = ""
+        for j in range(len(keywords_age)):
+            m = 0
+            nb = []
+            for k in range(len(keywords_age[j])):
+                nb.append(desc.count(keywords_age[j][k]))
+            m = max(nb)
+            if m > Mx:
+                Mx = m
+                age = age_range[j]
+        ar.append(age)
+    df["Age_range"] = ar
+    return df
+
+
+def main():
+    print("Reading the input")
+    print("")
+    df = pd.read_csv(r'Google-Playstore.csv')
+    # Filtering the dataset with educational apps respecting specific features
+    df_edu = filter(df)
+    # The dataset now only contains specific lines and has an index composed of the remaining rows' numbers
+    # Reinitialisation of the index to be [0,1,...,n]
+    df_edu = df_edu.set_index([pd.Index([i for i in range(len(df_edu))])])
+    # Selecting the educational games
+    df_edu_g = select_games(df_edu)
+    df_edu_g = df_edu_g.set_index([pd.Index([i for i in range(len(df_edu_g))])])
+    # Improving the dataset
+    # Add descriptions, number of reviews
+    df_edu_g = enrich_dataframe(df_edu_g)
+    # Use of NLP to add learning categories and age ranges
+    df_edu_g = find_lc(df_edu_g)
+    df_edu_g = find_age_range(df_edu_g)
+    df_edu_g = filter_non_enrichable(df_edu_g)
+    print(df_edu_g)
+    df_edu_g.to_csv(r'dataset_seriousgames.csv', index=False)
+
+main()
+
+
